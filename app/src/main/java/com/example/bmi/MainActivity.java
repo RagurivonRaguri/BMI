f90709288c26fa9c14f913ed00896a44e82c50f2
magda-devc <mgchebet@gmail.com>
2026-01-12 16:52:10 +0300

    Added Bmi calculator display

--- app/src/main/java/com/example/bmi/MainActivity.java
+++ app/src/main/java/com/example/bmi/MainActivity.java
@@ -7,6 +7,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,10 +19,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        EditText etWeight = findViewById(R.id.etWeight);
+        EditText etHeight = findViewById(R.id.etHeight);
+        Button btnCalculate = findViewById(R.id.btnCalculate);
+        TextView tvResult = findViewById(R.id.tvResult);
+
+        btnCalculate.setOnClickListener(v -> {
+            double weight = Double.parseDouble(etWeight.getText().toString());
+            double height = Double.parseDouble(etHeight.getText().toString());
+            double bmi = weight / (height * height);
+
+            tvResult.setText("BMI: " + bmi);
         });
     }
 }
